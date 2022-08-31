cf7ff3f351f29cdcea0c11d0e6fff9bee4a97a10
wwwwhddp <simran.abbasov.00@mail.ru>
2022-08-31 11:12:47 +0400

    Add project files.

---
+++ Class/CustomerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer ID: " + customer.id);
+            Console.WriteLine(customer.firstname + "  " + customer.lastname);
+            Console.WriteLine("-----Customer has been added!------");
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.firstname + "  " + customer.lastname);
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.id = 648;
+            customer1.firstname = "Jemy";
+            customer1.lastname = "Vardy";
+
+            Customer customer2 = new Customer();
+            customer2.id = 649;
+            customer2.firstname = "Nick";
+            customer2.lastname = "Border";
+
+            Customer customer3 = new Customer();
+            customer3.id = 650;
+            customer3.firstname = "Coby";
+            customer3.lastname = "Sam";
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("\n-----Customer has been listed!------");
+            customerManager.List(customer1);
+            customerManager.List(customer2);
+            customerManager.List(customer3);
+        }
+    }
+}
